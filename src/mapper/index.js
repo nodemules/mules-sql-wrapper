1f95677fcbc874fb284bc2d493b9b0898bf51bc3
brenthaertlein <brenthaertlein@gmail.com>
2017-10-11 16:31:46 -0400

    refactor mapper.parse functionality => mapper/parser.js

--- src/mapper/index.js
+++ src/mapper/index.js
@@ -1,80 +1,26 @@
 {
   const _ = require('lodash');
-  const moment = require('moment');
 
-  let allowEmptySchema = false;
-
-  let schema;
-
-  function parseModelFromRow(row) {
-    _.forEach(row, (v, k) => {
-      let type;
-      if (moment(v, moment.ISO_8601, true).isValid()) {
-        type = Date;
-      } else if (_.isFinite(parseInt(v))) {
-        type = Number;
-      }
-      let value = k;
-      if (type) {
-        value = {
-          'column': k,
-          'type': type
-        };
-      }
-      schema[_.camelCase(k)] = value;
-    });
-  }
-
-  function parseType(type, value) {
-    switch (type) {
-      case Number:
-        return parseInt(value);
-      case Date:
-        return new Date(value);
-      default:
-        return value;
-    }
-  }
-
-  function parse(row) {
-    let o = {};
-    if (_.isEmpty(schema)) {
-      if (!allowEmptySchema) {
-        throw 'A schema is required to parse a row. Use mapper.allowEmptySchema(true) to create a dynamic schema for given rows.';
-      }
-      parseModelFromRow(row);
-    }
-    _.forEach(schema, (v, k) => {
-      if (_.isString(v)) {
-        o[k] = row[v];
-      }
-      if (_.isObject(v)) {
-        o[k] = parseType(v.type, row[v.column]);
-      }
-    });
-    return o;
-  }
+  const Parser = require('./parser');
 
   module.exports = {
-    loadSchema: (s) => {
-      schema = s;
+    loadSchema: (schema) => {
+      Parser.setSchema(schema);
     },
     getSchema: () => {
-      return schema;
+      return Parser.getSchema();
     },
-    allowEmptySchema: (allow) => {
-      allowEmptySchema = allow;
-    },
+    allowEmptySchema: Parser.allowEmptySchema,
     parse: (rows) => {
       if (!rows || !rows.length) {
         return {};
       }
-      return parse(rows[0]);
+      return Parser.parse(rows[0]);
     },
     mapRows: (rows) => {
       let results = [];
       _.forEach(rows, (row) => {
-        results.push(parse(row));
+        results.push(Parser.parse(row));
       });
       return results;
     }
